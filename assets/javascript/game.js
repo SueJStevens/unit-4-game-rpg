b030b6641029951a73f8803816c92e2c7b8100e1
SueJStevens <StevensStock@gmail.com>
2018-09-04 20:49:28 -0700

    Working Model except for styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,17 +2,20 @@ var game = {
     states : [ "notStarted", "playerCharChosen", "opponentCharChosen", "battleInProgress", 
 "opponentDefeated", "gameOver" ],
     currentState : "NotStarted",
-    charactersAll : [ "Name1", "Name2",  "Name3", "Name4", "Name5" , "Name6" , "Name7" , "Name8" , "Name9" ],
+    charactersAll : [ "Nicole Kidman", "Name2",  "Name3", "Name4", "Name5" , "Name6" , "Name7" , "Name8" , "Name9" ],
     charNumToPlay: 4,
     characters : [ "Name1", "Name2",  "Name3", "Name4" ],
     healthArray : [ 1, 2, 3, 4 ],
     attackArray : [ ],
     counterAttackArray : [ ],
     player : "",
+    playerPoloroid : "",
     playerHealth : 0,
+    playerAttackBase : 0,
     playerAttack : 0,
     playerCounter : 0,
     opponent: "",
+    opponentPoloroid : "",
     opponentHealth : 0,
     opponentAttack : 0,
     opponentCounter : 0,
@@ -52,14 +55,6 @@ var game = {
         //when the page loads the first time:
         restart();
 
-    function elementToggle(elementIn, classIn, actionIn) {
-        if (actionIn === "add") {
-            $(elementIn).addClass(classIn);
-        } else if (actionIn === "show") {
-            $(elementIn).removeClass(classIn);
-        }
-    };
-
     function restart(){
         //hide containers where needed opponentListContainer
         elementToggle("#opponentListContainer", "d-none", "add");
@@ -76,22 +71,22 @@ var game = {
 
         
         //create random health power
-        for (var j=1; j<101; j++) {
+        for (var j=0; j<100; j++) {
             game.valDeck[j] = [j+100];
 //            console.log(game.valDeck[j]);
         }
         game.shuffle(game.valDeck);
 
         //create random attack power
-        for (var k=1; k<26; k++) {
-            game.valDeck1[k] = [k];
+        for (var k=0; k<25; k++) {
+            game.valDeck1[k] = [k+1];
  //           console.log(game.valDeck1[k]);
         }
         game.shuffle(game.valDeck1);
 
         //create random counter attack power
-        for (var l=1; l<26; l++) {
-            game.valDeck2[l] = [l];
+        for (var l=0; l<25; l++) {
+            game.valDeck2[l] = [l+1];
 //            console.log(game.valDeck2[l]);
         }
         game.shuffle(game.valDeck2);
@@ -110,8 +105,10 @@ var game = {
         createCharList("characterList", "character");
         //clear playerCircle
         game.player = "";
+        game.playerPoloroid = "";
         game.playerHealth = 0;
         game.playerAttack = 0;
+        game.playerAttackBase = 0;
         game.playerCounter = 0;
         createPlayerCircle(game.player, game.playerHealth, "notStarted");
 
@@ -126,39 +123,14 @@ var game = {
 
     };
 
-
-    //create on-click listener for the character class
-    $("#characterList").on("click",".character",function() {
-        //which character was clicked?
-        x = $(this).attr("data-character-index");
-        //add character to player variable 
-        game.player = game.characters[x];
-        //add character's health to playerHealth variable
-        game.playerHealth = game.healthArray[x];
-        game.playerAttack = game.attackArray[x];
-        game.playerCounter = game.counterAttackArray[x];
-        //add character to player circle
-        createPlayerCircle(game.player, game.playerHealth, "playerCharChosen");
-        //remove character for character list array
-        game.characters.splice(x,1);
-        //remove health Points for character in array
-        game.healthArray.splice(x,1);
-        game.attackArray.splice(x,1);
-        game.counterAttackArray.splice(x,1);
-        //recreate characterlist
-        createCharList("characterList", "character");
-        //create the List of Available Opponents
-        createCharList("opponentsAvailList", "opponent1")
-
-        //show Players Circle and Opponents List
-        elementToggle("#playerCircle", "d-none", "show");
-        elementToggle("#opponentListContainer", "d-none", "show");
- 
-        //hide Character List
-        elementToggle("#characterContainer", "d-none", "add");
-
-    });
-
+    //changes the class d-none so that the element either is hidden or not hidden
+    function elementToggle(elementIn, classIn, actionIn) {
+        if (actionIn === "add") {
+            $(elementIn).addClass(classIn);
+        } else if (actionIn === "show") {
+            $(elementIn).removeClass(classIn);
+        }
+    };
 
     //create character list html block
     function createCharList (elementIn, typeIn) {
@@ -194,19 +166,21 @@ var game = {
 
     }; //end function
 
-    function createPlayerCircle (player, phealth, gameStatusIn) {
+    function createPlayerCircle (player,phealth, gameStatusIn) {
         //remove anything previously created to get a clean slate for the dom.
         var list = document.getElementById("playerCircle");
         while (list.hasChildNodes()) {   
             list.removeChild(list.firstChild);
         }
 
+poloroid = "PoloroidFrame4.png";
+
         //construct the the html to append
         str = '<div class="col"> \
-                <h2 class="text-left"><strong>Star Character</strong></h2> \
+                <h2 class="text-left"><strong>Starlet</strong></h2> \
                 <div class="row"> \
                     <div class="col-2 text-center"> \
-                        <img class="img-responsive d-inline p-1 character" id="playerImg" src="assets/images/PoloroidFrame4.png" alt="' + player +'" value=1 /> \
+                        <img class="img-responsive d-inline p-1 character" id="playerImg" src="assets/images/' + poloroid + '" alt="' + player +'" value=1 /> \
                         <p class="m-0">' + player + '</p> \
                         <p class="m-0">HP: ' + phealth + '</p> \
                     </div> \
@@ -229,21 +203,64 @@ var game = {
         }
 
         //construct the html to append
-        str = '<p>'+who+'</p> \
-                    <img class="img-responsive d-inline p-1 ' + who +'" id="'+who+'Img" src="assets/images/PoloroidFrame4.png" alt="' + player +'" value=1 /> \
+        if(who === 'defender') {
+            battlelabel='Starlet';
+        } else {
+            battlelabel='b-list';
+        }
+
+        poloroid = "PoloroidFrame4.png";
+
+        str = '<p>'+ battlelabel +'</p> \
+                    <img class="img-responsive d-inline p-1 ' + who +'" id="'+who+'Img" src="assets/images/' + poloroid + '" alt="' + player +'" value=1 /> \
                         <p class="m-0">' + player + '</p> \
-                        <p class="m-0">HP: ' + phealth + '</p> \
+                        <p id="' +who+ '-hp" class="m-0">HP: ' + phealth + '</p> \
                     </div> \
                 </div> \
             </div>'
         
         $("#"+who).append(str);
 
+        //hide opponent list container
+        elementToggle("#opponentListContainer", "d-none", "add");
+
         //change game status
         gameStatus = gameStatusIn;
 
     }; //end function
 
+    //create on-click listener for the character class
+    $("#characterList").on("click",".character",function() {
+        //which character was clicked?
+        x = $(this).attr("data-character-index");
+        //add character to player variable 
+        game.player = game.characters[x];
+        //add character's health to playerHealth variable
+        game.playerHealth = game.healthArray[x];
+        game.playerAttack = game.attackArray[x];
+        game.playerAttackBase = game.attackArray[x];
+        game.playerCounter = game.counterAttackArray[x];
+        //add character to player circle
+        createPlayerCircle(game.player, game.playerHealth, "playerCharChosen");
+        //remove character for character list array
+        game.characters.splice(x,1);
+        //remove health Points for character in array
+        game.healthArray.splice(x,1);
+        game.attackArray.splice(x,1);
+        game.counterAttackArray.splice(x,1);
+        //recreate characterlist
+        createCharList("characterList", "character");
+        //create the List of Available Opponents
+        createCharList("opponentsAvailList", "opponent1")
+
+        //show Players Circle and Opponents List
+        elementToggle("#playerCircle", "d-none", "show");
+        elementToggle("#opponentListContainer", "d-none", "show");
+    
+        //hide Character List
+        elementToggle("#characterContainer", "d-none", "add");
+
+    });
 
     //create on-click listener for the opponent class
     $('#opponentsAvailList').on('click', '.opponent1', function() {
@@ -270,6 +287,99 @@ var game = {
         createCharList("opponentsAvailList", "opponent1")
         //show Battle Field and continue to show Opponents List; Character list still hidden
         elementToggle("#battlefield", "d-none", "show");
+        elementToggle("#attack", "d-none", "show");
+        //hide message board
+        elementToggle("#msgBoard", "d-none", "add");
+        //hide the player circle
+        elementToggle("#playerCircle", "d-none", "add");
+
+
+    });
+
+    $("#attack").on('click', '.attack', function() {
+        console.log("Attack Happened");
+
+        //math
+
+        //clear message board
+
+        //remove anything previously created to get a clean slate for the dom.
+        var list = document.getElementById("attackMsg");
+        while (list.hasChildNodes()) {   
+            list.removeChild(list.firstChild);
+        }
+
+        //create the message for the board
+         var msg = '<p>You attacked ' + game.opponent + ' for ' + game.playerAttack + ' damage.</p> \
+        <p>' + game.opponent + ' attacked you back for ' + game.opponentCounter + ' damage.</p>'
+
+        //calc health points
+        game.opponentHealth -= game.playerAttack;
+        game.playerHealth -= game.opponentCounter;
+
+        //test for death
+
+        switch(true) {
+            case (game.playerHealth <=0):
+                console.log ("Player Dead!");
+                //game over -- player dead
+                msg += '<p>You have been defeated ... game over</p>';
+                //hide the attack button
+                elementToggle("#attack", "d-none", "add");
+                break;
+            case (game.opponentHealth <=0):
+                console.log ("Opponent Dead!");
+                //game over -- player dead
+                //check to see if there are any other opponents
+                console.log(game.characters.length);
+                if(game.characters.length > 0) {
+                    msg += '<p>You have defeated ' + game.opponent + ', you can choose another enemy to fight.</p>';
+                    //hide the attack button
+                    elementToggle("#attack", "d-none", "add");
+                    //show opponent list container
+                    elementToggle("#opponentListContainer", "d-none", "show");
+
+                } else {
+                    msg += '<p>You are the winner!... game over</p>'
+                }
+                break;
+            default:
+                console.log("Keep Playing");
+        }
+
+        //if player dead -- end of game
+
+        //if opponent dead -- end of battle, pick another enemy
+
+        //game continues
+
+        //append the message
+        $("#attackMsg").append(msg);
+
+        //show the board (it may be showing or it may be hidden)
+        elementToggle("#msgBoard", "d-none", "show");
+
+        //update the health points, attack and counter attack variables
+        
+
+        game.playerAttack = parseInt(game.playerAttack) + parseInt(game.playerAttackBase);
+
+
+        
+        
+        //display new health points on the character
+        $('#defender-hp').text('hp: '+game.playerHealth);
+        //display new health points on the enemy
+        $('#enemy-hp').text('hp: '+game.opponentHealth);
+        //defender-hp
+        //enemy-hp
+
+
+
+
+//        <p>You have been defeated ... game over</p>
+// <p>You have defeated Enemy Name, you can choose another enemy to fight.</p>
+//<p>No enemy here.  You must choose an enemy to continue the game.</p> 
 
 
     });
